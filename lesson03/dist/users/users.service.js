2cf741c9674a332f0b1e42f140f423f3427dcd86
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-16 17:01:34 +0300

    Providers

--- lesson03/dist/users/users.service.js
+++ lesson03/dist/users/users.service.js
@@ -9,6 +9,38 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.UsersService = void 0;
 const common_1 = require("@nestjs/common");
 let UsersService = class UsersService {
+    users = [
+        {
+            id: 1,
+            name: 'Leanne Graham',
+            email: 'Sincere@april.biz',
+            role: 'INTERN',
+        },
+        {
+            id: 2,
+            name: 'Ervin Howell',
+            email: 'Shanna@melissa.tv',
+            role: 'INTERN',
+        },
+        {
+            id: 3,
+            name: 'Clementine Bauch',
+            email: 'Nathan@yesenia.net',
+            role: 'ENGINEER',
+        },
+        {
+            id: 4,
+            name: 'Patricia Lebsack',
+            email: 'Julianne.OConner@kory.org',
+            role: 'ENGINEER',
+        },
+        {
+            id: 5,
+            name: 'Chelsey Dietrich',
+            email: 'Lucio_Hettinger@annie.ca',
+            role: 'ADMIN',
+        },
+    ];
 };
 exports.UsersService = UsersService;
 exports.UsersService = UsersService = __decorate([
--- lesson03/src/users/users.service.ts
+++ lesson03/src/users/users.service.ts
@@ -1,4 +1,39 @@
+/* Providers
+Providers are a core concept in Nest. Many of the basic Nest classes, such as services, repositories, factories, and helpers, can be treated as providers. The key idea behind a provider is that it can be injected as a dependency, allowing objects to form various relationships with each other. The responsibility of "wiring up" these objects is largely handled by the Nest runtime system.*/
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      email: 'Sincere@april.biz',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      email: 'Shanna@melissa.tv',
+      role: 'INTERN',
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      email: 'Nathan@yesenia.net',
+      role: 'ENGINEER',
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      email: 'Julianne.OConner@kory.org',
+      role: 'ENGINEER',
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      email: 'Lucio_Hettinger@annie.ca',
+      role: 'ADMIN',
+    },
+  ];
+}
